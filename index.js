1177699ff418ce6e0f7b74c5002854aac9090e15
mattscilipoti <matt.scilipoti@ga.co>
2015-11-24 11:03:54 -0500

    sync code with Express lesson

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.get("/", function(req,res){
 });
 
 app.get("/:numberOfBottles?", function( req, res ){
-  var bottles = req.params.numberOfBottles || 99;
-  var next = bottles - 1;
-  if (bottles > 1){
-    res.send(bottles + " bottles of beer on the wall <a href='/" + next + "'>Take one down pass it around");
+  var numberOfBottles = req.params.numberOfBottles || 99;
+  var next = numberOfBottles - 1;
+  if (numberOfBottles > 1){
+    res.send(numberOfBottles + " bottles of beer on the wall <a href='/" + next + "'>Take one down pass it around");
   }
   else{
     res.send("1 bottle of beer on the wall <a href='/'>Start Over</a>");
