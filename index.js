df4f23a2a5713e691b50504ac25d6ec29af591e5
mattscilipoti <matt.scilipoti@ga.co>
2015-11-24 11:22:46 -0500

    utilize controllers/bottles.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 var express = require("express");
 var app = express();
 var hbs = require("hbs");
+var bottles = require("./controllers/bottles");
+
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public')); // Serve static content (css, etc) for the app from the “public” directory in the application directory
+
 
 app.listen(3000, function(){
   console.log("app listening on port " + this.address().port);
 });
 
-app.get("/:numberOfBottles?", function( req, res ){
-  var numberOfBottles = req.params.numberOfBottles || 99;
-  var next = numberOfBottles - 1;
-  res.render("index", {bottles: numberOfBottles, next: next});
-});
+app.get("/:numberOfBottles?", bottles.index);
