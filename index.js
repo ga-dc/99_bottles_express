2a798a46d5db8026be9c27755b56aff4621a8773
Tory Burgett <vdburgett@gmail.com>
2015-11-24 16:02:52 -0500

    Indented

--- index.js
+++ index.js
@@ -11,24 +11,24 @@ app.get("/", function(req, res){
 
 app.get("/:num", function(req, res){
   if (isNaN(req.params.num) === false){
-  if (parseInt(req.params.num) === 99){
-    res.send("<p>99 bottles of beer on the wall</p><a href=\"/98\"> take one down, pass it around... </a>");
-  }
-  else if (parseInt(req.params.num) === 98){
-    res.send("<p>"+req.params.num+" bottles of beer on the wall</p><a href=\"/"+(req.params.num-1)+"\"> take one down, pass it around... </a> <br> <a href=\"/\"> take that bottle back, put it back up... </a>");
-  }
-  else if (parseInt(req.params.num) === 0){
-    res.send("<p>"+req.params.num+" bottles of beer on the wall. Party\'s Over.</p><a href=\"/\"> Start Over? </a>");
-  }
-  else if (parseInt(req.params.num) > 99){
-    res.send("<p>"+req.params.num+" bottles of beer?? You can't have that many!! What parties are you having?!</p><a href=\"/\"> Go back to 99 Bottles Start... </a>");
-  }
-  else if (parseInt(req.params.num) < 0){
-    res.send("<p>"+req.params.num+" bottles of beer? You can't have negative beer. Only regret and hangovers.</p><a href=\"/\"> Go back to 99 Bottles Start... </a>");
-  }
-  else{
-    res.send("<p>"+req.params.num+" bottles of beer on the wall</p><a href=\"/"+(req.params.num-1)+"\"> take one down, pass it around... </a> <br> <a href=\""+( parseInt(req.params.num) + 1)+"\"> take that bottle back, put it back up... </a>");
-  }
+    if (parseInt(req.params.num) === 99){
+      res.send("<p>99 bottles of beer on the wall</p><a href=\"/98\"> take one down, pass it around... </a>");
+    }
+    else if (parseInt(req.params.num) === 98){
+      res.send("<p>"+req.params.num+" bottles of beer on the wall</p><a href=\"/"+(req.params.num-1)+"\"> take one down, pass it around... </a> <br> <a href=\"/\"> take that bottle back, put it back up... </a>");
+    }
+    else if (parseInt(req.params.num) === 0){
+      res.send("<p>"+req.params.num+" bottles of beer on the wall. Party\'s Over.</p><a href=\"/\"> Start Over? </a>");
+    }
+    else if (parseInt(req.params.num) > 99){
+      res.send("<p>"+req.params.num+" bottles of beer?? You can't have that many!! What parties are you having?!</p><a href=\"/\"> Go back to 99 Bottles Start... </a>");
+    }
+    else if (parseInt(req.params.num) < 0){
+      res.send("<p>"+req.params.num+" bottles of beer? You can't have negative beer. Only regret and hangovers.</p><a href=\"/\"> Go back to 99 Bottles Start... </a>");
+    }
+    else{
+      res.send("<p>"+req.params.num+" bottles of beer on the wall</p><a href=\"/"+(req.params.num-1)+"\"> take one down, pass it around... </a> <br> <a href=\""+( parseInt(req.params.num) + 1)+"\"> take that bottle back, put it back up... </a>");
+    }
   }
   else{
     res.send("<p>"+req.params.num+"?? You can't have that! The rules are VERY simple. We're asking for NUMBERS. PLAY BY THE RULES. </p><a href=\"/\"> Back to 99 Bottles of Beer on the wall... </a>");
